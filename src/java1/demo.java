94412d8e3fdf81f57676c539f35009b5cc22e9e0
ACARPAN <123221549+ACARPAN@users.noreply.github.com>
2023-01-27 11:32:02 -0800

    demo1

---
+++ src/java1/demo.java
@@ -0,0 +1,17 @@
+package java1;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =100000;
+		int y =2000000;
+		int z =300000000;
+		System.out.println(x);
+		
+		
+		
+
+	}
+
+}
