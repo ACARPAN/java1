6f4d33302ed064b168a05eafe1d345b624a9a784
ACARPAN <123221549+ACARPAN@users.noreply.github.com>
2023-02-01 19:51:06 -0800

    string

---
+++ src/string1/string01.java
@@ -0,0 +1,12 @@
+package string1;
+
+public class string01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String lesson = " I am starting to learn a selenium";
+System.out.println(lesson); 
+
+	}
+
+}
