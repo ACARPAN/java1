3559ff9ac6f9eadef4be9442ed7e4fbe35b13b38
ACARPAN <123221549+ACARPAN@users.noreply.github.com>
2023-03-15 15:45:33 -0700

    added

---
+++ src/interlink/Enum03.java
@@ -0,0 +1,10 @@
+package interlink;
+
+public class Enum03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/interlink/Enum2.java
@@ -0,0 +1,29 @@
+package interlink;
+
+enum Vehicles {
+
+	// enum constants calling with constructor;
+
+	SUV("Mercedes"), SEDAN("Tesla 3"), COUPE("Porsche");
+
+	private final String sizeCar;
+
+	private Vehicles(String c) {
+		this.sizeCar = c;
+	}
+
+	public String getSizes() {
+		return this.sizeCar;
+	}
+
+}
+
+public class Enum2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehicles a = Vehicles.COUPE;
+		System.out.println(a.getSizes());
+	}
+
+}
